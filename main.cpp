1eeb0103e4d0b288348c9f35f14ce0a8e0b912f6
ThomasDev82 <lancevancy7@gmail.com>
2022-08-11 02:06:47 +0200

    Problem - B

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // This program is written by Thomas
+
+    int i;
+    long long l;
+    char c;
+    float f;
+    double d;
+
+    cin >> i >> l >> c >> f >> d ;
+
+    cout << i << endl;
+    cout << l << endl;
+    cout << c << endl;
+    cout << f << endl;
+    cout << d << endl;
+
+
+    return 0;
+}
